c3609376ecdd959982dadbfa5747a2f7182ddbe2
Kurosaki H'lls <141827510+Itsmeji@users.noreply.github.com>
2024-05-23 01:19:26 +0000

    Update settings.js

--- settings.js
+++ settings.js
@@ -11,7 +11,7 @@ const fs = require('fs')
 const chalk = require('chalk')
 
 //session
-global.sessionid ='ur session id'
+global.sessionid ='creds.json'
 
 //owmner v card
 global.ytname = "YT: Xeon" //ur yt chanel name
